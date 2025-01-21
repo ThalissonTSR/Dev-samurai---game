973111d629ac781e56887c0713e9fb4263846d5b
ThalissonTSR <thalissontharl@gmail.com>
2025-01-21 17:19:46 -0300

    inicio do jogo

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let folhaDesenho = document.getElementById("folha");
+var folhaDesenho = document.getElementById("folha");
 
-let areaDesenho = folhaDesenho.getContext("2d");
+var areaDesenho = folhaDesenho.getContext("2d");
 
+let larguraCampo = 600;
+let alturaCampo = 500;
+let larguraLinha = 5;
+let larguraBola = 5;
+let alturaRaquete = 50;
+
+//background
 areaDesenho.fillStyle = "#286047";
-areaDesenho.fillRect(0,0,600,500);
+areaDesenho.fillRect(0,0,larguraCampo,alturaCampo);
 
-areaDesenho.fillStyle = "#ffffff";
-areaDesenho.fillRect(297.5,0,5,500);
+//barra meio
+areaDesenho.fillStyle = "white";
+areaDesenho.fillRect(larguraCampo/2 - larguraLinha/2,0,larguraLinha,alturaCampo);
+
+//raquete 1
+areaDesenho.fillRect(0,30,larguraLinha,alturaRaquete);
+
+//raquete 2
+areaDesenho.fillRect(larguraCampo- larguraLinha,30,larguraLinha,alturaRaquete);
+
+//bola
+areaDesenho.fillRect(30,30,larguraBola,larguraBola);
+
