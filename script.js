072b739c27e63873acfaa036b29edd32132f629c
ThalissonTSR <thalissontharl@gmail.com>
2025-01-21 16:20:06 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+let folhaDesenho = document.getElementById("folha");
+
+let areaDesenho = folhaDesenho.getContext("2d");
+
+areaDesenho.fillStyle = "#286047";
+areaDesenho.fillRect(0,0,600,500);
+
+areaDesenho.fillStyle = "#ffffff";
+areaDesenho.fillRect(297.5,0,5,500);
